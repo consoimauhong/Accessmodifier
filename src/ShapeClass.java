8fea5088f205b7d879a9db607d82e26b02b46469
Your Name <you@example.com>
2022-12-13 13:43:26 +0700

    new

---
+++ src/ShapeClass.java
@@ -0,0 +1,32 @@
+public class ShapeClass {
+    private String color = "green";
+    private boolean filled = true;
+
+    public ShapeClass(){
+
+    }
+    public ShapeClass(String color, boolean filled){
+        this.color = color;
+        this.filled = filled;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public boolean isFilled(){
+        return filled;
+    }
+    public void setFilled(boolean filled){
+        this.filled = filled;
+    }
+    @Override
+    public  String toString(){
+        return "A Shape width color of "
+                + getColor()
+                + " and "
+                + (isFilled() ? "filled" : " not filled");
+    }
+
+}
---
+++ src/ShapeTest.java
@@ -0,0 +1,9 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+        ShapeClass shapeclass = new ShapeClass();
+        System.out.println(shapeclass);
+
+        shapeclass = new ShapeClass("red", false);
+        System.out.println(shapeclass);
+    }
+}
